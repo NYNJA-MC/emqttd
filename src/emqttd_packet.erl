85ae19fbf45e10a168dae0d238a0cbe4aa8a1e22
Ulf Norell <ulf.norell@gmail.com>
2020-03-06 14:25:05 +0100

    Don't dump large payloads to log

--- src/emqttd_packet.erl
+++ src/emqttd_packet.erl
@@ -67,7 +67,7 @@ format_variable(undefined, _) ->
 format_variable(Variable, undefined) ->
     format_variable(Variable);
 format_variable(Variable, Payload) ->
-    io_lib:format("~s, Payload=~p", [format_variable(Variable), Payload]).
+    io_lib:format("~s, Payload=~W", [format_variable(Variable), Payload, 32]).
 
 format_variable(#mqtt_packet_connect{
                  proto_ver     = ProtoVer,
